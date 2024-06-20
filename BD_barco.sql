4d9a4190d99a06bd61934985fb725ac64ea3966e
Allan Couto <allan.leste@hotmail.com>
2024-06-20 13:04:40 -0300

    bd

---
+++ BD_barco.sql
@@ -0,0 +1,8 @@
+create database barco;
+use barco;
+
+create table contatos(
+nome varchar(255) not null,
+email varchar(255) not null,
+cpf varchar(255) not null,
+duvidas varchar(255) not null);
--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect 
+
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.secret_key = "12345678"
+
+app.config['MYSQL_HOST'] = 'localhost' #'127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'calculadora'
+app.config['MYSQL_DB'] = 'barco'
+mysql = MySQL(app)
 
 @app.route("/")
 def homepage():
@@ -26,6 +35,24 @@ def modelo2():
 def modelo3():
     return render_template("modelo3.html")
 
+@app.route("/AV-4")
+def modelo4():
+    return render_template("modelo4.html")
+
+@app.route("/enviar",methods=['POST'])
+def enviar(): 
+    name= request.form.get('fnome')
+    email= request.form.get('femail')
+    cpf= request.form.get('CPF')
+    fdescricao= request.form.get('fdescricao')
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO contatos (nome, email, cpf, duvidas) VALUES (%s, %s, %s, %s)", (name, email, cpf, fdescricao))
+    mysql.connection.commit()
+    cur.close()
+    return redirect('/')
+
+
 
 if __name__== "__main__":
     app.run(debug=True)
+    
